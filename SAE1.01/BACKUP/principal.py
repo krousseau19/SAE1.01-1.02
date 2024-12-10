a4ee7cee940496dd90442a2a2ea958f9ed4dc000
Jules87000 <jules.gayet12@gmail.com>
2024-12-10 10:32:34 +0100

    Add files via upload

---
+++ SAE1.01/BACKUP/principal.py
@@ -0,0 +1,155 @@
+# Fichier assemblant les différents modules et leur fonctions respectives pour permettre à l'application d'être éxécutée correctement
+from devinette import jeu_devinette, saisir_intervalle
+from allumette import jeu_allumettes
+from morpion import jeu_morpion
+from puissance4 import jeu_puissance4
+from ressource import creation_joueurs, afficher_menu, saisir_choix, j1, j2, afficher_stats, f, sauvegarder_joueur, afficher_leaderboard, Joueur
+
+def restart(jeu : str, j1 : Joueur, j2 : Joueur) -> bool :
+    """
+    Entrée : 3 arguments, une chaîne de caractère correspondant au jeu, et 2 joueurs
+
+    Sortie : Un booléen (pour savoir si les joueurs veulent rejouer ou non)
+
+    Fonctionnement : La fonction affiche un menu en continue et demande si le joueur veut refaire une partie ou non,
+    si la réponse est oui, la fonction lance le jeu passé en paramètre, et renvoie True, sinon elle ramène au menu principal
+    et renvoie False
+    """
+    choix : int
+    intervalle = int
+
+    choix = -1
+    restart = False
+    while choix != 1 and choix != 2 :
+        print("Voulez vous refaire une partie ?")
+        print("1 - Oui")
+        print("2 - Non")
+        choix = saisir_choix()
+        if choix == 1 :
+            input("Une nouvelle partie va commencer, veuillez appuyer sur ENTRER...")
+            if jeu == "Allumette" :
+                jeu_allumettes(j1, j2)
+            elif jeu == "Morpion" :
+                jeu_morpion(j1, j2)
+            elif jeu == "Puissance4" :
+                jeu_puissance4(j1, j2)
+            else :
+                intervalle = saisir_intervalle()
+                jeu_devinette(j1, j2, intervalle)
+            restart = True
+        elif choix == 2 :
+            input("Veuillez appuyer sur ENTRER pour revenir au menu principal...")
+        else :
+            print("\033c")
+            print("\x1b[31mErreur : Choix invalide\x1b[0m")
+            
+    return restart
+
+#Programme principal de l'application (Menu et lancement des jeux)
+if __name__ == "__main__" :
+    menu_actif : bool
+    menu_stats_actif : bool
+    choix : int
+    sous_choix : int
+    intervalle : int
+    rejoue : bool 
+
+    menu_actif = True
+    rejoue = True
+    creation_joueurs(j1, j2, f)
+    while menu_actif :
+        print("\033c")
+        afficher_menu()
+        choix = saisir_choix()
+        if choix == 1 :
+            rejoue = True
+            intervalle = saisir_intervalle()
+            jeu_devinette(j1, j2, intervalle)
+            sauvegarder_joueur(f, j1)
+            sauvegarder_joueur(f, j2)
+            while rejoue :
+                rejoue = restart("Devinette", j1, j2)
+                sauvegarder_joueur(f, j1)
+                sauvegarder_joueur(f, j2)
+        elif choix == 2 :
+            rejoue = True
+            jeu_allumettes(j1, j2)
+            sauvegarder_joueur(f, j1)
+            sauvegarder_joueur(f, j2)
+            while rejoue :
+                rejoue = restart("Allumette", j1, j2)
+                sauvegarder_joueur(f, j1)
+                sauvegarder_joueur(f, j2)
+        elif choix == 3 :
+            rejoue = True
+            jeu_morpion(j1, j2)
+            sauvegarder_joueur(f, j1)
+            sauvegarder_joueur(f, j2)
+            while rejoue :
+                rejoue = restart("Morpion", j1, j2)
+                sauvegarder_joueur(f, j1)
+                sauvegarder_joueur(f, j2)
+        elif choix == 4 :
+            rejoue = True
+            jeu_puissance4(j1, j2)
+            sauvegarder_joueur(f, j1)
+            sauvegarder_joueur(f, j2)
+            while rejoue :
+                rejoue = restart("Puissance4", j1, j2)
+                sauvegarder_joueur(f, j1)
+                sauvegarder_joueur(f, j2)
+        elif choix == 5 :
+            menu_stats_actif = True
+            print("\033c")
+            while menu_stats_actif :
+                print("\033c")
+                print("    === Statistiques ===")
+                print("1 - LeaderBoard")
+                print("2 - Infos sur les joueurs")
+                print("3 - Quitter les Statistiques")
+                sous_choix = saisir_choix()
+                if sous_choix == 1 :
+                    sous_choix = -1
+                    print("\033c")
+                    while sous_choix != 5 :
+                        print("\033c")
+                        print("    === LeaderBoard ===")
+                        print("1 - Devinettes")
+                        print("2 - Allumettes")
+                        print("3 - Morpion")
+                        print("4 - Puissance 4")
+                        print("5 - Quitter le LeaderBoard")
+                        sous_choix = saisir_choix()
+                        if sous_choix == 1 :
+                            print("\033c")
+                            afficher_leaderboard(f, "Devinette")
+                        elif sous_choix == 2 :
+                            print("\033c")
+                            afficher_leaderboard(f, "Allumette")
+                        elif sous_choix == 3 :
+                            print("\033c")
+                            afficher_leaderboard(f, "Morpion")
+                        elif sous_choix == 4 :
+                            print("\033c")
+                            afficher_leaderboard(f, "Puissance 4")
+                        elif sous_choix == 5 :
+                            print("\033c")
+                        else :
+                            print("\033c")
+                            print("\x1b[31mErreur : Choix invalide.\x1b[0m")
+                elif sous_choix == 2 :
+                    print("\033c")
+                    afficher_stats(f, j1, j2)
+                elif sous_choix == 3 :
+                    print("\033c")
+                    menu_stats_actif = False
+                else :
+                    print("\033c")
+                    print("\x1b[31mErreur : Choix invalide.\x1b[0m")
+        elif choix == 6 :
+            print("\033c")
+            print("Au revoir !")
+            menu_actif = False
+        else :
+            print("\033c")
+            print("\x1b[31mErreur : Choix invalide.\x1b[0m")
---
+++ SAE1.01/BACKUP/puissance4.py
@@ -0,0 +1,166 @@
+# Fichier contenant toutes les fonctions relatives au jeu de puissance 4
+from ressource import Joueur, début_de_partie, qui_joue
+
+def check_victoire(grille : list[list[str]], symbole : str) -> bool :
+    """
+    Entrée : Tableau de tableau (3x3) contenant des chaînes de caractères, et le symbole du joueur
+
+    Sortie : Un booléen, True si il y a une victoire, False sinon
+
+    Fonctionnement : Utilise 2 boucles imbriquées pour parcourir les lignes, les colonnes, et les diagonales afin de
+    trouver un paterne contenant 4 fois d'affilé le symbole du joueur, dans ce cas, la fonction renvoie True, sinon il n'y
+    a pas de victoire donc la fonction renvoie False
+    """
+    lignes : int
+    colonnes : int
+
+    lignes = len(grille)
+    colonnes = len(grille[0])
+    # Vérifie les lignes
+    for i in range(lignes):
+        for j in range(colonnes - 3):  # seulement jusqu'à la 4e dernière colonne (suite de 4 jetons et évite IndexError)
+            if grille[i][j] == grille[i][j+1] == grille[i][j+2] == grille[i][j+3] == symbole:
+                return True
+    # Vérifie les colonnes
+    for j in range(colonnes):
+        for i in range(lignes - 3):  # seulement jusqu'à la 4e dernière ligne (suite de 4 jetons et évite IndexError)
+            if grille[i][j] == grille[i+1][j] == grille[i+2][j] == grille[i+3][j] == symbole:
+                return True
+    # Vérifie les diagonales (haut-gauche vers bas-droite)
+    for i in range(lignes - 3):
+        for j in range(colonnes - 3): # //
+            if grille[i][j] == grille[i+1][j+1] == grille[i+2][j+2] == grille[i+3][j+3] == symbole:
+                return True
+    # Vérifie les diagonales (bas-gauche vers haut-droite)
+    for i in range(3, lignes):
+        for j in range(colonnes - 3): # //
+            if grille[i][j] == grille[i-1][j+1] == grille[i-2][j+2] == grille[i-3][j+3] == symbole:
+                return True
+    return False
+
+def check_égalité(grille : list[list[str]]) -> bool :
+    """
+    Entrée : Un tableau de tableau (3x3) contenant des chaînes de caractères
+
+    Sortie : Un booléen, True si il y a égalité, False sinon
+
+    Fonctionnement : Parcours chaque ligne du tableau, si une case vide est trouvée alors la fonction renvoie
+    False, sinon si elle n'en trouve pas, alors le tableau et plein, il y a égalité, et elle renvoie True
+    """
+    ligne : list[str]
+    # Parcours chaque ligne de la grille
+    for ligne in grille:
+        if " " in ligne:  # Si une case vide est trouvée, donc la grille n'est pas pleine
+            return False
+    return True  # Aucune case vide n'a été trouvée, donc la grille est pleine
+
+def afficher_grille(grille : list[list[str]]):
+    """
+    Entrée : Un tableau de tableau (3x3) contenant des chaînes de caractères
+
+    Sortie : Rien
+
+    Fonctionnement : Affiche la grille passée en paramètre avec en jointure des " | " pour faire les séparateurs
+    de colonnes, une fois cela fait, la fonction affiche les indicces de chaque colonne en dessous de celles-ci
+    """
+    ligne : list[str]
+
+    ligne = []
+    for ligne in grille:
+        print(" | ".join(ligne))    # séparateurs de colonnes
+    print("1 | 2 | 3 | 4 | 5 | 6 | 7")  # indices des colonnes
+
+def jeu_puissance4(j1 : Joueur, j2 : Joueur):
+    """
+    Entrée : 2 arguments, les 2 joueurs
+
+    Sortie : Rien
+
+    Fonctionnement : Fonction qui gère les principales fonctionnalités du puissance 4, et qui fait aussi appel aux autres fonctions la précédant. 
+    Elle initialise les variables concernant le jeu (la grille, le symbole, les booléens de contrôle, etc...). Une fois cela fait, tant que la partie
+    n'est pas finie, la fonction affiche la grille et demande un saisi à l'utilisateur, puis modifie la grille en conséquence. A chaque tour la fonction
+    effectue les différents tests de victoire/égalité et affiche les messages en fonction des résultats, ainsi que la modification du booléen partie_finie
+    si nécessaire. Une fois la partie finie, les attributs des joueurs sont modifiés selon leur score et la fonction s'arrête
+    """
+    joueur : Joueur
+    grille : list[list[str]]
+    symbole : str
+    partie_finie : bool
+    colonne_pleine : bool
+    saisi_j : bool
+    colonne : int
+    ligne : int
+
+    print("\033c")
+    print("=== Jeu de Puissance 4 ===")
+    grille = [[" " for _ in range(7)] for _ in range(6)]    # 7 colonnes, 6 lignes
+    joueur = début_de_partie(j1, j2, qui_joue)
+    j1.score = 0
+    j2.score = 0
+    ligne = -1
+    colonne = -1
+    partie_finie = False
+    
+    while not partie_finie :
+        if joueur == j1 :
+            symbole = '\x1b[31mO\x1b[37m'
+        else :
+            symbole = '\x1b[38;5;226mO\x1b[37m'
+        saisi_j = False
+        while not saisi_j :
+            afficher_grille(grille)
+            try:
+                colonne = int(input(f"{joueur.pseudo} ({symbole}), choisissez une colonne (1-7) : "))
+                print("\033c")
+                saisi_j = True
+            except ValueError:
+                print("\033c")
+                print("\x1b[31mErreur : Veuillez entrer un nombre valide.\x1b[0m")
+                saisi_j = False
+            if colonne < 1 or colonne > 7:
+                print("\033c")
+                print("\x1b[31mErreur : Numéro de colonne invalide, réessayez.\x1b[0m")
+                saisi_j = False
+            if saisi_j :
+                colonne_pleine = True
+                for ligne in range(5, -1, -1):
+                    if grille[ligne][colonne-1] == " "  and colonne_pleine == True :
+                        grille[ligne][colonne-1] = symbole
+                        joueur.score += 5
+                        colonne_pleine = False
+            
+                if colonne_pleine :
+                    print("\033c")
+                    print("\x1b[31mErreur : Cette colonne est pleine, choisissez-en une autre.\x1b[0m")
+                    saisi_j = False
+        saisi_j = False       
+        if check_victoire(grille, symbole):
+            print("\033c")
+            afficher_grille(grille)
+            print(f"\x1b[32mFélicitations ! {joueur.pseudo} a gagné !")
+            print("Votre score : ", joueur.score, "\x1b[0m")
+            joueur.nb_partieG += 1
+            joueur.score += 1
+            # Mise à jour score gagnant
+            if joueur.score > joueur.highscore_pui :
+                    joueur.highscore_pui = joueur.score
+            #Mise à jour score perdant
+            if joueur == j1 :
+                joueur = j2
+            else :
+                joueur = j1
+            if joueur.score > joueur.highscore_pui :
+                    joueur.highscore_pui = joueur.score
+            partie_finie = True
+        elif check_égalité(grille):
+            print("\033c")
+            afficher_grille(grille)
+            print("\x1b[38;5;208mIl y a eu égalité ! La grille est pleine !\x1b[0m")
+            partie_finie = True
+        else : 
+            if joueur == j1 :
+                joueur = j2
+            else :
+                joueur = j1
+    j1.nb_partie += 1
+    j2.nb_partie += 1
